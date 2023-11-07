78800280715637245f343ca5796d5267f8081330
tausifc2004 <tac46@njit.edu>
2023-11-07 02:04:12 -0500

    added player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
     public GameObject level1Wall;
@@ -50,10 +52,22 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+
+        Vector3 jump = new Vector3(movementX, 2.0f, movementY);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)

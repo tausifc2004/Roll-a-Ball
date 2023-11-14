11f3e93020ef1a9c55959c45363d883a88b676f8
tausifc2004 <tac46@njit.edu>
2023-11-14 05:29:32 -0500

    added timer and game over screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private int count;
+    public int count;
     private float movementX;
     private float movementY;
     public float speed = 0;
@@ -15,9 +16,11 @@ public class PlayerController : MonoBehaviour
     public bool isGrounded;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    public GameObject retryTextObject;
     public GameObject level1Wall;
     public GameObject Player;
     public GameObject PickupSound;
+    [SerializeField] private GameObject gameOverMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -42,7 +45,10 @@ public class PlayerController : MonoBehaviour
         countText.text = "Count: " + count.ToString();
         if (count >= 16)
         {
+            gameOverMenu.SetActive(true);
             winTextObject.SetActive(true);
+            retryTextObject.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 
@@ -74,6 +80,15 @@ public class PlayerController : MonoBehaviour
         if (Player.transform.position.y <= -45)
         {
             Player.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
